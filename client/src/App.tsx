3338546c65460e770320ee842078bf6720e9fe6f
Maxime Parriaux <maxime.parriaux@epitech.eu>
2021-03-22 13:43:46 +0100

    Add Beers !

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -6,7 +6,7 @@ import Login from "./scopes/Login/Login";
 import "./App.css";
 import { AppProvider, useAppContext } from "./contexts/AppContext";
 import TokenHandler from "./scopes/TokenHandler/TokenHandler";
-import Trade from "./scopes/Trade/Trade";
+import Beers from "./scopes/Beers/Beers";
 
 // The famous nullable boolean we inherited from Java
 type nullableBoolean = boolean | null;
@@ -24,19 +24,19 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h5>{`Can't stop, won't stop,`}</h5>
         <img
-          src="https://cdn.worldvectorlogo.com/logos/gamestop.svg"
+          src="https://www.picng.com/upload/beer/png_beer_10372.png"
           className="App-logo"
           alt="logo"
         />
+        <h4>Smartbeers</h4>
       </header>
       <Router>
         <Switch>
           <Route path="/login" component={Login}></Route>
           {token && (
             <>
-              <Route path="/trade" component={Trade}></Route>
+              <Route path="/beers" component={Beers}></Route>
             </>
           )}
           <Route path="*" exact>
