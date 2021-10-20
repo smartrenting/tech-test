76020a7148839e6aefd745d9a17343600fb9dfb7
JohnPAfr <john.aurry@gmail.com>
2021-10-20 17:06:27 +0200

    presave orders in db

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const loginRouter = require('./routes/login');
 const orderRouter = require('./routes/order');
 const { url } = require('./config/db.config');
 
+const OrderModel = require('./models/order.model');
+
 const port = process.env.LOCAL_PORT || 4242;
 const app = express();
 
@@ -28,7 +30,67 @@ mongoose
   });
 
 app.get('/hello', (req, res) => {
-  res.send('hello');
+  OrderModel.find({}, (err, data) => {
+    if (err) res.send(err);
+    else if (!data.length) {
+      const orders = [
+        {
+          username: 'John',
+          quantity: 2,
+          date: new Date(),
+        },
+        {
+          username: 'John',
+          quantity: 1,
+          date: new Date(),
+        },
+        {
+          username: 'John',
+          quantity: 3,
+          date: new Date(),
+        },
+        {
+          username: 'Doe',
+          quantity: 1,
+          date: new Date(),
+        },
+        {
+          username: 'Doe',
+          quantity: 1,
+          date: new Date(),
+        },
+        {
+          username: 'Doe',
+          quantity: 3,
+          date: new Date(),
+        },
+        {
+          username: 'Ginette',
+          quantity: 2,
+          date: new Date(),
+        },
+        {
+          username: 'Bob',
+          quantity: 3,
+          date: new Date(),
+        },
+        {
+          username: 'Ginette',
+          quantity: 6,
+          date: new Date(),
+        },
+        {
+          username: 'Bob',
+          quantity: 10,
+          date: new Date(),
+        },
+      ];
+      OrderModel.create(orders, () => {
+        if (err) res.send(err);
+        else res.send('hello');
+      });
+    } else res.send('hello');
+  });
 });
 
 app.use(loginRouter);
